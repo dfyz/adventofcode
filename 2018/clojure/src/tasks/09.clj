deffb33a74f4d3669e2e43dc930ff8ee9d2b76b6
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-01-04 01:22:16 +0300

    Task #09 was waaaaaay too slow

--- 2018/clojure/src/tasks/09.clj
+++ 2018/clojure/src/tasks/09.clj
@@ -1,43 +1,39 @@
 (ns tasks.09
   (:require [clojure.data.finger-tree :as ft]))
 
-(defn step-simple [nums idx next-marble]
-  (let [split-pos (rem (inc idx) (count nums))
-        idx-next (inc split-pos)
-        [before middle after] (ft/ft-split-at nums split-pos)
-        nums-next (ft/ft-concat (conj (conj before middle) next-marble) after)]
-    [nums-next idx-next 0]))
+(defn rotate-left [nums]
+  (conj (rest nums) (first nums)))
 
-(defn step-complex [nums idx next-marble]
-  (let [to-remove-idx (rem
-                        (+ (count nums) (- idx 7))
-                        (count nums))
-        [before removed after] (ft/ft-split-at nums to-remove-idx)
-        nums-next (ft/ft-concat before after)
-        idx-next (rem to-remove-idx (count nums-next))]
-    [nums-next idx-next (+ removed next-marble)]))
+(defn rotate-right [nums]
+  (ft/conjl (pop nums) (peek nums)))
 
-(defn step [nums idx next-marble]
+(defn step-simple [nums next-marble]
+  (let [nums-next (ft/conjl (rotate-left (rotate-left nums)) next-marble)]
+    [nums-next 0]))
+
+(defn step-complex [nums next-marble]
+  (let [rotated (nth (iterate rotate-right nums) 7)]
+    [(rest rotated) (+ next-marble (first rotated))]))
+
+(defn step [nums next-marble]
   (let [step-func (if (zero? (rem next-marble 23))
                     step-complex step-simple)]
-    (step-func nums idx next-marble)))
+    (step-func nums next-marble)))
 
 (defn play [turn-count player-count]
   (let [scores (vec (repeat player-count 0))]
     (loop [marble 1
            scores scores
            nums (ft/counted-double-list 0)
-           idx 0
            player-idx 0]
       (if (> marble turn-count)
         scores
-        (let [[nums-next idx-next bonus] (step nums idx marble)
+        (let [[nums-next bonus] (step nums marble)
               score-next (+ bonus (get scores player-idx))]
           (recur
             (inc marble)
             (assoc scores player-idx score-next)
             nums-next
-            idx-next
             (rem (inc player-idx) (count scores))))))))
 
 (def sample [[25 9] [1618 10] [7999 13] [1104 17] [6111 21] [5807 30]])
