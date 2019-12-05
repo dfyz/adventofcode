c7fbe0b6e36b664b8b627ba8a80e81971b8d0a77
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-12-06 01:23:02 +0300

    Preliminary implementation of conditions

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -17,14 +17,20 @@ class Program:
                 2: self.mul,
                 3: self.read_input,
                 4: self.print_output,
+                5: self.jump_if_true,
+                6: self.jump_if_false,
+                7: self.less_than,
+                8: self.equals,
             }[opcode % 100]
 
             operand_count = len(inspect.signature(op).parameters)
             operands = self.data[self.ip + 1:self.ip + 1 + operand_count]
             modes = reversed(str(opcode)[:-2])
             parsed_operands = [(o, m == '1') for o, m in itertools.zip_longest(operands, modes)]
-            ip_delta = op(*parsed_operands)
-            self.ip += ip_delta if ip_delta is not None else 1 + operand_count
+            if (new_ip := op(*parsed_operands)) is not None:
+                self.ip = new_ip
+            else:
+                self.ip += 1 + operand_count
 
     def add(self, in1, in2, out):
         self.set(out, self.get(in1) + self.get(in2))
@@ -38,6 +44,20 @@ class Program:
 
     def print_output(self, in1):
         self.outputs.append(self.get(in1))
+
+    def jump_if_true(self, in1, in2):
+        if self.get(in1) != 0:
+            return self.get(in2)
+
+    def jump_if_false(self, in1, in2):
+        if self.get(in1) == 0:
+            return self.get(in2)
+
+    def less_than(self, in1, in2, out):
+        self.set(out, int(self.get(in1) < self.get(in2)))
+
+    def equals(self, in1, in2, out):
+        self.set(out, int(self.get(in1) == self.get(in2)))
 
     def get(self, operand):
         pos, mode = operand
