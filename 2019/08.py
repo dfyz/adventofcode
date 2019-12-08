6fa20dc83ecffd6f167bacd4adabb861f8fa10b6
Ivan Komarov <Ivan.Komarov@dfyz.info>
2019-12-08 09:01:15 +0300

    08 hard

--- 2019/08.py
+++ 2019/08.py
@@ -15,9 +15,24 @@ def solve_easy(data):
     return min_layer.count('1') * min_layer.count('2')
 
 
+def solve_hard(data):
+    def combine_pixels(p1, p2):
+        return p1 if p1 != '2' else p2
+
+    def combine_layers(l1, l2):
+        return [combine_pixels(p1, p2) for p1, p2 in zip(l1, l2)]
+
+    final_layer = data[0]
+    for layer in data[1:]:
+        final_layer = combine_layers(final_layer, layer)
+
+    return '\n'.join(''.join(final_layer[start:start + WIDTH]) for start in range(0, len(final_layer), WIDTH)).replace('0', ' ')
+
+
 def main():
     data = parse_image(Path('08.txt').read_text().strip())
     print(solve_easy(data))
+    print(solve_hard(data))
 
 
 if __name__ == '__main__':
