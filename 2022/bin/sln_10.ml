f77f54cd42d9e8418dd4042709a047ffdbac6e46
Ivan Komarov <Ivan.Komarov@dfyz.info>
2022-12-11 03:30:43 +0100

    10 hard

--- 2022/bin/sln_10.ml
+++ 2022/bin/sln_10.ml
@@ -8,26 +8,49 @@ let input =
         )
         raw_input
 
-let solve_easy =
-    let rec simulate insns cycle value acc =
-        let update_acc cycle acc =
-            acc +
-                (if (cycle - 20) mod 40 = 0 && cycle <= 220
-                then cycle * value
-                else 0)
-        in
+let simulate callback =
+    let rec impl insns cycle value =
         match insns with
-        | [] -> acc
+        | [] -> ()
         | (insn :: rest) ->
             match insn with
                 | Some delta ->
-                    let acc1 = update_acc cycle acc in
-                    let acc2 = update_acc (cycle + 1) acc1 in
-                    simulate rest (cycle + 2) (value + delta) acc2
+                    callback cycle value;
+                    callback (cycle + 1) value;
+                    impl rest (cycle + 2) (value + delta)
                 | None ->
-                    let acc1 = update_acc cycle acc in
-                    simulate rest (cycle + 1) value acc1
+                    callback cycle value;
+                    impl rest (cycle + 1) value
     in
-    let ans = simulate input 1 1 0 in
-    Printf.printf "easy: %d\n" ans
+    impl input 1 1
 
+let solve_easy =
+    let res = ref 0 in
+    let callback cycle value =
+        if (cycle - 20) mod 40 = 0 && cycle <= 220
+        then
+            let strength = cycle * value in
+            res := !res + strength
+    in
+    simulate callback;
+    Printf.printf "easy: %d\n" !res
+
+let rows = 6
+let cols = 40
+
+let solve_hard =
+    let res = Array.make_matrix rows cols '.' in
+    let callback cycle value =
+        let zc = cycle - 1 in
+        let (draw_r, draw_c) = (zc / cols, zc mod cols) in
+        if List.mem draw_c [value - 1; value; value + 1]
+        then res.(draw_r).(draw_c) <- '#'
+    in
+    simulate callback;
+    print_endline "hard: ";
+    for r = 0 to rows - 1 do
+        for c = 0 to cols - 1 do
+            print_char res.(r).(c)
+        done;
+        print_newline ()
+    done
