a6c2929385044daf64caf4b4e70bd040e727f05a
Ivan Komarov <Ivan.Komarov@dfyz.info>
2022-12-11 02:42:56 +0100

    09 hard

--- 2022/bin/sln_09.ml
+++ 2022/bin/sln_09.ml
@@ -31,17 +31,32 @@ let move_tail (hx, hy) ((tx, ty) as tail_pos) =
             (tx + move dx abs_dx, ty + move dy abs_dy)
         | _ -> failwith "invalid head/tail configuration"
 
-let solve_easy =
-    let rec make_move (dir, rem) locs head_pos tail_pos =
-        let new_locs = Locs.add tail_pos locs in    
-        if rem <= 0 then (new_locs, head_pos, tail_pos)
+let move_rope rope dir =
+    let res = Array.copy rope in
+    res.(0) <- move_head res.(0) dir;
+    for i = 1 to (Array.length rope - 1) do
+        res.(i) <- move_tail res.(i - 1) res.(i)
+    done;
+    res
+
+let solve_rope rope_len =
+    let rec make_move (dir, rem) locs rope =
+        let cur_tail = rope.(Array.length rope - 1) in
+        let new_locs = Locs.add cur_tail locs in
+        if rem <= 0 then (new_locs, rope)
         else
-            let new_head = move_head head_pos dir in
-            let new_tail = move_tail new_head tail_pos in
-            make_move (dir, rem - 1) new_locs new_head new_tail
+            let new_rope = move_rope rope dir in
+            make_move (dir, rem - 1) new_locs new_rope
     in
-        let (all_locs, _, _) = List.fold_left (fun (locs, head_pos, tail_pos) line ->
-            make_move line locs head_pos tail_pos
-        ) (Locs.empty, (0, 0), (0, 0)) input
+        let (all_locs, _) = List.fold_left
+            (fun (locs, rope) line ->
+                make_move line locs rope
+            )
+            (Locs.empty, Array.make rope_len (0, 0))
+            input
     in
-    Printf.printf "easy: %d\n" (Locs.cardinal all_locs)
+    Locs.cardinal all_locs
+
+let _ =
+    Printf.printf "easy: %d\n" (solve_rope 2);
+    Printf.printf "hard: %d\n" (solve_rope 10)
