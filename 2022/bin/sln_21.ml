bf06943170e524abd3328393b68355826f8a8011
Ivan Komarov <Ivan.Komarov@dfyz.info>
2022-12-22 02:43:33 +0100

    21 hard

--- 2022/bin/sln_21.ml
+++ 2022/bin/sln_21.ml
@@ -37,6 +37,14 @@ let input =
         );
     instructions
 
+let safe_div x y = assert (x mod y = 0); x / y
+
+let eval_op res1 op res2 = match op with
+    | Add -> res1 + res2
+    | Sub -> res1 - res2
+    | Mul -> res1 * res2
+    | Div -> safe_div res1 res2
+
 let eval human_val =
     let rec impl monkey =
         match Hashtbl.find input monkey with
@@ -46,13 +54,51 @@ let eval human_val =
         | Combine (monkey1, op, monkey2) ->
             let res1 = impl monkey1 in
             let res2 = impl monkey2 in
-            match op with
-                | Add -> res1 + res2
-                | Sub -> res1 - res2
-                | Mul -> res1 * res2
-                | Div -> assert (res1 mod res2 = 0); res1 / res2
+            eval_op res1 op res2
     in
     impl "root"
 
 let solve_easy =
     Printf.printf "easy: %d\n" (eval None)
+
+type eval_tree =
+    | Const of int
+    | Placeholder
+    | Op of eval_tree * op_type * eval_tree
+
+let rec calc_eval_tree monkey =
+    if monkey = "humn"
+    then
+        Placeholder
+    else
+        match Hashtbl.find input monkey with
+            | Yell num -> Const num
+            | Combine (monkey1, op, monkey2) ->
+                let res1 = calc_eval_tree monkey1 in
+                let res2 = calc_eval_tree monkey2 in
+                match (res1, res2) with
+                    | (Const a, Const b) -> Const (eval_op a op b)
+                    | (a, b) -> Op (a, op, b)
+
+let solve_hard =
+    let rec solve tree target = match tree with
+        | Placeholder -> target
+        | _ ->
+            let (new_tree, x, op, x_on_left) = match tree with
+                | Op (Const x, op, y) -> (y, x, op, true)
+                | Op (y, op, Const x) -> (y, x, op, false)
+                | _ -> failwith "Oh noes"
+            in
+            let new_target = match op with
+                | Add -> target - x
+                | Sub -> (if x_on_left then (-) else (+)) x target
+                | Mul -> safe_div target x
+                | Div -> (if x_on_left then safe_div else ( * )) x target
+            in
+            solve new_tree new_target
+    in
+    let ans = match calc_eval_tree "root" with
+        | Op (tree, Add, Const target) -> solve tree target
+        | _ -> failwith "Uh oh"
+    in
+    Printf.printf "hard: %d\n" ans
