d44b603f12713308efc4a1b86ed62b2e510030ad
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-12-11 14:46:38 +0100

    better

--- 2023/11/main.go
+++ 2023/11/main.go
@@ -8,6 +8,38 @@ import (
 
 type P = image.Point
 
+func solve(galaxies []P, expandRows, expandCols []bool, isHard bool) int {
+	expansionFactor := 2
+	if isHard {
+		expansionFactor = 1000000
+	}
+	getDist := func(src, dst P) int {
+		rDist := 0
+		for coord := min(src.Y, dst.Y); coord < max(src.Y, dst.Y); coord++ {
+			if expandRows[coord] {
+				rDist += expansionFactor - 1
+			}
+			rDist++
+		}
+		cDist := 0
+		for coord := min(src.X, dst.X); coord < max(src.X, dst.X); coord++ {
+			if expandCols[coord] {
+				cDist += expansionFactor - 1
+			}
+			cDist++
+		}
+		return rDist + cDist
+	}
+
+	res := 0
+	for ii := range galaxies {
+		for jj := ii + 1; jj < len(galaxies); jj++ {
+			res += getDist(galaxies[ii], galaxies[jj])
+		}
+	}
+	return res
+}
+
 func main() {
 	lines := common.ReadLines("11/input.txt")
 	rows, cols := len(lines), len(lines[0])
@@ -27,43 +59,15 @@ func main() {
 		}
 	}
 
-	galaxyMap := make(map[P]bool)
+	galaxies := make([]P, 0)
 	for rr := 0; rr < rows; rr++ {
 		for cc := 0; cc < cols; cc++ {
 			if lines[rr][cc] == '#' {
-				galaxyMap[P{cc, rr}] = true
+				galaxies = append(galaxies, P{cc, rr})
 			}
 		}
 	}
 
-	galaxies := make([]P, 0)
-	for k, _ := range galaxyMap {
-		galaxies = append(galaxies, k)
-	}
-
-	getDist := func(src, dst P) int {
-		rDist := 0
-		for coord := min(src.Y, dst.Y); coord < max(src.Y, dst.Y); coord++ {
-			if expandRows[coord] {
-				rDist += 1000000 - 1
-			}
-			rDist++
-		}
-		cDist := 0
-		for coord := min(src.X, dst.X); coord < max(src.X, dst.X); coord++ {
-			if expandCols[coord] {
-				cDist += 1000000 - 1
-			}
-			cDist++
-		}
-		return rDist + cDist
-	}
-
-	easy := 0
-	for ii := range galaxies {
-		for jj := ii + 1; jj < len(galaxies); jj++ {
-			easy += getDist(galaxies[ii], galaxies[jj])
-		}
-	}
-	fmt.Printf("easy: %d\n", easy)
+	fmt.Printf("easy: %d\n", solve(galaxies, expandRows, expandCols, false))
+	fmt.Printf("hard: %d\n", solve(galaxies, expandRows, expandCols, true))
 }
